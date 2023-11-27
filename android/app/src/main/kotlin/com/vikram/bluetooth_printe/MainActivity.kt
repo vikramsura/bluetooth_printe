79bdb491949869ce995f2c0566be75842a78521c
vikram <vikramsura35@gmail.com>
2023-11-27 22:18:47 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/vikram/bluetooth_printe/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.vikram.bluetooth_printe
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/printPage.dart
@@ -0,0 +1,134 @@
+import 'package:bluetooth_print/bluetooth_print.dart';
+import 'package:bluetooth_print/bluetooth_print_model.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class PrintPage extends StatefulWidget {
+  final List<Map<String, dynamic>> data;
+
+  PrintPage(this.data);
+
+  @override
+  _PrintPageState createState() => _PrintPageState();
+}
+
+class _PrintPageState extends State<PrintPage> {
+  BluetoothPrint bluetoothPrint = BluetoothPrint.instance;
+  List<BluetoothDevice> devices = [];
+  String _devicesMsg = "";
+  final f = NumberFormat("\$###,###.00", "en_US");
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) => {initPrinter()});
+  }
+
+  Future<void> initPrinter() async {
+    bluetoothPrint.startScan(timeout: Duration(seconds: 4));
+    bluetoothPrint.isScanning;
+    if (!mounted) return;
+    bluetoothPrint.scanResults.listen(
+      (val) {
+        if (!mounted) return;
+        setState(() {
+          devices = val;
+        });
+        if (devices.isEmpty)
+          setState(() {
+            _devicesMsg = "No Devices";
+          });
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Select Printer'),
+        backgroundColor: Colors.redAccent,
+      ),
+      body: devices.isEmpty
+          ? Center(
+              child: Text(_devicesMsg ?? ''),
+            )
+          : ListView.builder(
+              itemCount: devices.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: Icon(Icons.print),
+                  title: Text(devices[index].name.toString()),
+                  subtitle: Text(devices[index].address.toString()),
+                  onTap: () {
+                    _startPrint(devices[index]);
+                    print("object${_startPrint(devices[index])}");
+                  },
+                );
+              },
+            ),
+      floatingActionButton: StreamBuilder<bool>(
+        stream: bluetoothPrint.isScanning,
+        initialData: false,
+        builder: (c, snapshot) {
+          if (snapshot.data == true) {
+            return FloatingActionButton(
+              child: Icon(Icons.stop),
+              onPressed: () => bluetoothPrint.stopScan(),
+              backgroundColor: Colors.red,
+            );
+          } else {
+            return FloatingActionButton(
+                child: Icon(Icons.search),
+                onPressed: () =>
+                    bluetoothPrint.startScan(timeout: Duration(seconds: 4)));
+          }
+        },
+      ),
+    );
+  }
+
+  Future<void> _startPrint(BluetoothDevice device) async {
+    if (device != null && device.address != null) {
+      await bluetoothPrint.connect(device);
+
+      Map<String, dynamic> config = Map();
+      List<LineText> list = [];
+
+      list.add(
+        LineText(
+          type: LineText.TYPE_TEXT,
+          content: "Grocery App",
+          weight: 2,
+          width: 2,
+          height: 2,
+          align: LineText.ALIGN_CENTER,
+          linefeed: 1,
+        ),
+      );
+
+      for (var i = 0; i < widget.data.length; i++) {
+        list.add(
+          LineText(
+            type: LineText.TYPE_TEXT,
+            content: widget.data[i]['title'],
+            weight: 0,
+            align: LineText.ALIGN_LEFT,
+            linefeed: 1,
+          ),
+        );
+
+        list.add(
+          LineText(
+            type: LineText.TYPE_TEXT,
+            content:
+                "${f.format(this.widget.data[i]['price'])} x ${this.widget.data[i]['qty']}",
+            align: LineText.ALIGN_LEFT,
+            linefeed: 1,
+          ),
+        );
+      }
+    }
+  }
+}
